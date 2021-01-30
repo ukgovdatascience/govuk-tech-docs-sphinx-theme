cb55a0e8099fae7083dfbbce10d5c6ea87fcf154
ESKYoung <ESKYoung@users.noreply.github.com>
2021-01-30 20:55:35 +0000

    Revise `header_logo.html`
    Include options to change department, project name, and project phase.

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ source_suffix = {
 master_doc = "index"
 
 # General information about the project.
-project = "GOV.UK Tech Docs Sphinx Theme"
+project = "Tech Docs Sphinx Theme"
 author = "ukgovdatascience"
 
 # The version info for the project you're documenting, acts as replacement for |version| and |release|, also used in
@@ -131,7 +131,10 @@ html_context = {
 
 # Theme options are theme-specific and customize the look and feel of a theme further.  For a list of options available
 # for each theme, see the documentation.
-# html_theme_options = {}
+html_theme_options = {
+    "department": "GOV.UK",
+    "phase": "Discovery"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [".."]
