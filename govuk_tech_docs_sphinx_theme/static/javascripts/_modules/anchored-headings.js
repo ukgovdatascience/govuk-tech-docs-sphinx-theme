7559b87fdfb7a20628a1a91ebbe82b8d58cedc75
ESKYoung <ESKYoung@users.noreply.github.com>
2021-02-03 17:40:08 +0000

    Amend `anchored-headings.js` to correctly render <a> tag

--- govuk_tech_docs_sphinx_theme/static/javascripts/_modules/anchored-headings.js
+++ govuk_tech_docs_sphinx_theme/static/javascripts/_modules/anchored-headings.js
@@ -11,7 +11,7 @@
       var $this = $(this)
       $this.addClass('anchored-heading')
       $this.prepend(
-        '<a href="#' + $this.attr('id') + '" class="anchored-heading__icon" aria-hidden="true" tabindex="-1"></a>'
+        '<a href="#' + $this.parents('div').attr('id') + '" class="anchored-heading__icon" aria-hidden="true" tabindex="-1"></a>'
       )
     }
   }
