c116ed91ec878576762b4212c0ca2d6ece2e63f5
ESKYoung <ESKYoung@users.noreply.github.com>
2021-04-05 18:37:14 +0100

    Correct broken link for anchor heading icons

--- govuk_tech_docs_sphinx_theme/static/javascripts/theme.js
+++ govuk_tech_docs_sphinx_theme/static/javascripts/theme.js
@@ -4,3 +4,10 @@ $(function() {
     $(this).css('width', '50%');
   });
 });
+
+// Sets the correct link for the anchored heading icon
+$(function() {
+  $('section').find('h1.anchored-heading, h2.anchored-heading, h3.anchored-heading, h4.anchored-heading, h5.anchored-heading, h6.anchored-heading').each(function() {
+    $(this).find('a.anchored-heading__icon').attr('href', '#' + $(this).parents('section').attr('id'));
+  });
+});
